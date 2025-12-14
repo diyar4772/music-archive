b4e86fac7c72c1bfd3d0098797f3323594812b0f
diyar4772 <samedyolcu7247@gmail.com>
2025-12-14 13:55:15 +0300

    Remove deprecated Mongoose options

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ app.use(express.static('.'));
 app.use(express.json());
 
 // --- MongoDB Connection ---
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('✅ MongoDB Connected!'))
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('✅ MongoDB Connected!'))
     .catch(err => console.error('❌ MongoDB Connection Error:', err.message));
 
 // --- Mongoose Schemas ---
